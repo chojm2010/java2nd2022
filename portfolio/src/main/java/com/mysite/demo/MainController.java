99ba8f17c6697a8270d46f52b637496032ee0fd3
chojm2010 <projm2786@gmail.com>
2022-05-16 18:19:25 +0900

    test

--- portfolio/src/main/java/com/mysite/demo/MainController.java
+++ portfolio/src/main/java/com/mysite/demo/MainController.java
@@ -1,13 +1,18 @@
 package com.mysite.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mysite.demo.question.Board;
 
 @Controller
 public class MainController {
 	@RequestMapping("/")
-	public String index() {
-		return "redirect:/question/list";
+	public String index(Model model) {
+		Board board = new Board(1, "타임리프 테스트1", "ㅋㅋㅋ");
+		model.addAttribute("board", board);
+		return "thymeLeafTest";
 	}
 }
